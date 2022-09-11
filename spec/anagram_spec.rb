2d2423793ff4bb8ef0b621b8422cac88b27daf2d
evegyzbirun <zbirunzbirun@gmail.com>
2022-09-10 20:14:51 -0700

    adding tests.

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,2 +1,11 @@
 require 'rspec'
 require ("./lib/anagram.rb")
+
+describe('#anagram?') do
+  it("is it anagram?") do
+    test_subject1 = "Lives"
+    test_subject2 = "Evils"
+    my_pal = Palindrome.new(test_subject)
+    expect(my_pal.is_palindrome?).to(eq(true))
+  end 
+end
