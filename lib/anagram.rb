b6106e48b885bf2453e2e6c60f1251f59bea48bf
evegyzbirun <zbirunzbirun@gmail.com>
2022-09-11 00:18:08 -0700

    still working

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,7 +3,7 @@ require 'pry'
 # "These words are anagrams." different cases but should still be anagrams. "Tea" is still an anagram of "Eat". 
 # Add a rule to check if the inputs are words.
 # "pkmn" isn't an anagram of "kmpn" because "pkmn" isn't a real word.) A word must contain a vowel (a, i, e, o, u) or y. 
-# "These words have no letter matches and are antigrams.  "hi" has no matches with "bye".
+# "These words have no letter matches and are antigrams.  "hi" has no matches with "bye". "These words have no letter matches and are antigrams."
 # Spaces and punctuation shouldn't count (so they should be removed).
 # =~ /[aeiou]/
 # binding.pry
@@ -15,25 +15,19 @@ require 'pry'
 class Input 
   attr_reader :inputOne, :inputTwo
   def initialize(inputOne, inputTwo)
-    @inputOne = inputOne
-    @inputTwo = inputTwo
+    @inputOne = inputOne.gsub(/[^A-Za-z0-9]/, '').downcase
+    @inputTwo = inputTwo.gsub(/[^A-Za-z0-9]/, '').downcase
   end
   def anagram?
- 
-    array_one_check = @inputOne.gsub(/[^A-Za-z0-9]/, '').downcase
-    array_two_check = @inputTwo.gsub(/[^A-Za-z0-9]/, '').downcase
-    
-  
-    
-   
-     if (array_one_check == array_two_check) 
-      
-      return "It is anagram."
-
-     else 
-      return "It is antigram."
-  
-     end
+    if (@inputOne =~ /[aeiou]/) && (@inputTwo =~ /[aeiou]/)
+      return "These inputs are not real words."
+    elsif()
+      return "These inputs are antigrams."
+    elsif()
+      return "These words have no letter matches and are antigrams."
+    else ()
+      return "These inputs are not real words."
+    end
     
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -6,7 +6,7 @@ describe('#anagram?') do
     test_subject1 = "Lives"
     test_subject2 = "Evils"
     my_ana = Input.new(test_subject1, test_subject2)
-    expect(my_ana.anagram?).to(eq("It is anagram."))
+    expect(my_ana.anagram?).to(eq("These inputs are anagrams."))
   end 
 end
 
@@ -15,7 +15,7 @@ describe('#anagram?') do
     test_subject1 = "Lites"
     test_subject2 = "Evils"
     my_ana = Input.new(test_subject1, test_subject2)
-    expect(my_ana.anagram?).to(eq("It is antigram."))
+    expect(my_ana.anagram?).to(eq("These inputs are antigrams."))
   end 
 end
 
@@ -24,6 +24,15 @@ describe('#anagram?') do
     test_subject1 = "sv"
     test_subject2 = "vs"
     my_ana = Input.new(test_subject1, test_subject2)
-    expect(my_ana.anagram?).to(eq("It is antigram."))
+    expect(my_ana.anagram?).to(eq("These inputs are not real words."))
+  end 
+end
+
+describe('#anagram?') do
+  it("it is antigram if no vowels") do
+    test_subject1 = "sav"
+    test_subject2 = "vfss"
+    my_ana = Input.new(test_subject1, test_subject2)
+    expect(my_ana.anagram?).to(eq("These words have no letter matches and are antigrams."))
   end 
 end
